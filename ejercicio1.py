6529a7aa5cc78c9aecf203c878d387d815edda08
cortezmarjho-gif <ac0427032026@unab.edu.sv>
2026-03-21 21:52:50 -0600

    ejercicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+# Solicitar nombre completo
+nombre = input("Ingrese su nombre completo: ")
+
+# Mostrar en mayúsculas
+print("Nombre en mayúsculas:", nombre.upper())
+
+# Mostrar en minúsculas
+print("Nombre en minúsculas:", nombre.lower())
+
+# Cantidad de caracteres
+print("Cantidad de caracteres:", len(nombre))
